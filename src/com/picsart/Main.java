638632f226cb0f4cdcb58e2540df1a04b3f5bd50
SevakGitRepo <sevo85tovm@gmail.com>
2021-06-21 22:08:07 +0400

    Next

--- src/com/picsart/Main.java
+++ src/com/picsart/Main.java
@@ -9,42 +9,64 @@ public class Main {
     static long fileCount;
     static long foldrCount;
     static long fileSize;
-    public static void main(String[] args) throws InterruptedException{
+
+    public static void main(String[] args) throws InterruptedException {
 
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the directory path");
         String path = scanner.nextLine();
 
-       // String path = "C:\\Windows\\WinSxS";
+        //String path = "C:\\Windows";
 
 
         final File folder = new File(path);
         System.out.println(folder.getName());
 
-
+        //listFilesForFolderNull(folder);
+        //2 method
         Thread thread = new Thread(() -> listFilesForFolder(folder));
         thread.start();
 
-        while (thread.isAlive()){
+        while (thread.isAlive()) {
             TimeUnit.MILLISECONDS.sleep(500);
             System.out.println("File count  = " + fileCount);
             System.out.println("Folder count = " + foldrCount);
-            System.out.println("Folder size = " + fileSize/1_000_000+" MB");
+            System.out.println("Folder size = " + fileSize / 1_000_000 + " MB");
             System.out.println("----------------------");
         }
+
         System.out.println("End");
     }
+
     public static void listFilesForFolder(final File folder) {
-        for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
-            if (fileEntry.isDirectory()) {
-                listFilesForFolder(fileEntry);
-                foldrCount++;
-            } else {
+        try {
+            for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
+                if (fileEntry.isDirectory()) {
+                    listFilesForFolder(fileEntry);
+                    foldrCount++;
+                } else {
 //                System.out.println(fileEntry.getName());
-                fileSize += fileEntry.length();
-                fileCount++;
+                    fileSize += fileEntry.length();
+                    fileCount++;
+                }
+            }
+        } catch (Exception ignored) {
+        }
+    }
+
+    public static void listFilesForFolderNull(final File folder) {
+        if (folder != null) {
+            for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
+                if (fileEntry.isDirectory()) {
+                    listFilesForFolder(fileEntry);
+                    foldrCount++;
+                } else {
+                    fileSize += fileEntry.length();
+                    fileCount++;
+                }
             }
         }
     }
+
 }
